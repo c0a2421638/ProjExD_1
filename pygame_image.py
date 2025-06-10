34cc94d693f0d95294a587621e676558b56a7059
Mao Fukunaga <c0a2421638@edu.teu.ac.jp>
2025-06-10 16:22:46 +0900

    練習問題6回目

--- pygame_image.py
+++ pygame_image.py
@@ -10,19 +10,30 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock  = pg.time.Clock()
     bg_img = pg.image.load("fig/pg_bg.jpg")
-    kou_img = pg.image.load("fig/3.png")
-    kou_img = pg.transform.flip(kou_img,True,False)
     bg_img2 =pg.transform.flip(bg_img,True,False)
+    kk_img = pg.image.load("fig/3.png")  # こうかとん画像のSurface
+    kk_img = pg.transform.flip(kk_img, True, False)
+    kk_rct = kk_img.get_rect() #こうかとんのrect画像
+    kk_rct.center = 300,200
     tmr = 0
     while True:
         for event in pg.event.get():
             if event.type == pg.QUIT: return
 
+        key_lst = pg.key.get_pressed() 
+        if key_lst[pg.K_UP]:  
+            kk_rct.move_ip(0, -1)  # 上に移動
+        if key_lst[pg.K_DOWN]:
+            kk_rct.move_ip(0, +1)
+        if key_lst[pg.K_LEFT]:
+            kk_rct.move_ip(-1, 0)
+        if key_lst[pg.K_RIGHT]:
+            kk_rct.move_ip(+1, 0)
         tmr %= 3199
         screen.blit(bg_img, [-tmr, 0])
         screen.blit(bg_img2, [1600-tmr, 0])
         screen.blit(bg_img, [3200-tmr, 0])
-        screen.blit(kou_img,[300,200])
+        screen.blit(kk_img,[300,200])
         pg.display.update()
         tmr += 1        
         clock.tick(200)
